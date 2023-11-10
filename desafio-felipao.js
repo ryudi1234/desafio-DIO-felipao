5b483623f3bd183c87b1808c4ca2b219bbe46f4e
ryudi1234 <ryudikitano@gmail,com>
2023-11-10 20:23:03 -0300

    atualização do varificador de rank

--- desafio-felipao.js
+++ desafio-felipao.js
@@ -1,16 +1,16 @@
 let name = prompt("qual o seu nome???")
 let jogadorNvl = parseInt(prompt("qunto de XP vc tem??"))
-
+debugger
 if(jogadorNvl <= 1000){
     alert("ferro")
 }
-else if(jogadorNvl > 1000 && jogadorNvl < 2000){
+else if(jogadorNvl > 1000 && jogadorNvl <= 2000){
     alert("bronze")
 }
-else if(jogadorNvl > 2000 && jogadorNvl < 5000){
+else if(jogadorNvl > 2000 && jogadorNvl <= 5000){
     alert("prata")
 }
-else if(jogadorNvl > 6000 && jogadorNvl < 7000){
+else if(jogadorNvl <= 6000 && jogadorNvl <= 7000){
     alert("ouro")
 }
 else if(jogadorNvl > 7000 && jogadorNvl < 8000){
@@ -22,10 +22,9 @@ else if(jogadorNvl > 8000 && jogadorNvl < 9000){
 else if(jogadorNvl > 9000 && jogadorNvl < 10000){
     alert("imortal")
 }
-else
-{
+else{
     alert("radiante")
-}
+};
 
 alert("o Heroi de nome " + name + " esta no nivel" + jogadorNvl)
 
